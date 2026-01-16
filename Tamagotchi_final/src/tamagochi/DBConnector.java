b3b6090de803cd007b5813651a842a668cde21f3
holyB2 <ksh7022@ewha.ac.kr>
2026-01-16 13:03:57 +0900

    Update DBConnector.java

--- Tamagotchi_final/src/tamagochi/DBConnector.java
+++ Tamagotchi_final/src/tamagochi/DBConnector.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 //DB에 연결만
 public class DBConnector {
-	private static final String URL ="jdbc:mysql://localhost:3306/tamagochi";
-	private static final String USER = "Testuser";
-	private static final String PASS = "testuser";
+	private static final String URL ="jdbc:mysql://...:3306/tamagochi";
+	private static final String USER = "...";
+	private static final String PASS = "...";
 
 	public Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER, PASS);
